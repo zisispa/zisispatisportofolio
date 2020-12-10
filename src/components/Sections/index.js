d09961629beaedc35dad85524371857eb87ec57c
Zisis Spatis <31807214+zisispa@users.noreply.github.com>
2020-12-10 10:33:02 +0200

    delete all

--- src/components/Sections/index.js
+++
@@ -1,3 +0,0 @@
-export { default as HeroSection } from "./HeroSection/HeroSection";
-export { default as AboutUsSection } from "./AboutUsSection/AboutUsSection";
-export { default as ProjectsSection } from "./ProjectsSection/ProjectsSection";
